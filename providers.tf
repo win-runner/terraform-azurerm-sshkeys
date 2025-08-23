4b54438259aeb6153a2790bd6ad629cfa32b0744
Friderici, Joshua <joshua.friderici@sva.de>
2025-08-23 11:33:47 +0200

    chore: updated provider constraints

--- providers.tf
+++ providers.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 4.0, < 5.0.0"
+      version = "~> 4.3"
     }
     tls = {
       source  = "hashicorp/tls"
-      version = ">= 4.1, < 5.0.0"
+      version = "~> 4.1"
     }
   }
 }
